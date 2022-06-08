0193addb645c7efe94da03a9df57a74fc98ed034
Aluno SENAI LAURO <aluno@SENAI-LAURO.LOCAL>
2022-06-07 21:32:44 -0300

    Changes js, html, css

--- src/js/multiPlayer.js
+++ src/js/multiPlayer.js
@@ -27,7 +27,11 @@ var valueChoiced2Player2 = Number(player2Num2.value)
 var player2Num3 = document.querySelector('.player2Num3')
 var valueChoiced3Player2 = Number(player2Num3.value)
 
-//Funções ativar e desativar
+var numeroPrincipal = 0
+
+var sectionWinner = document.querySelector('#sec2')
+
+//Criando funções
 function desligarBut1 (){
     player1Num1.disabled = true
     player1Num2.disabled = true
@@ -53,101 +57,119 @@ function ativarBut2 (){
 }
 
 
-//Funçoes de click
-
-pagSingle.onclick = function(){
-
-        
-
-}
-
-
-
-player1.onclick = function(){
-
-    desligarBut1()
-    ativarBut2()
-
-    player1Num1.setAttribute('value', `${valueChoiced1Player1 + 1}`)
-    player1Num2.setAttribute('value', `${valueChoiced1Player1 + 2}`)
-    player1Num3.setAttribute('value', `${valueChoiced1Player1 + 3}`)
-
-    
-}
-
-
-player2.onclick = function(){
-    
-    desligarBut2()
-    ativarBut1()
-
-
-    player2Num1.setAttribute('value', `${valueChoiced1Player1 += 1}`)
-    player2Num2.setAttribute('value', `${valueChoiced1Player1 += 2}`)
-    player2Num3.setAttribute('value', `${valueChoiced1Player1 += 3}`)
-
-}
-
-
-//Clicks player 1
-/*player1Num1.addEventListener('click', () => {
-
-    player1Num1.disabled = false
-    player1Num2.disabled = false
-    player1Num3.disabled = false
-
-    player2Num1.disabled = true
-    player2Num2.disabled = true
-    player2Num3.disabled = true
-
+function somarButtons(){
     player1Num1.setAttribute('value', `${valueChoiced1Player1 += 1}`)
     player1Num2.setAttribute('value', `${valueChoiced2Player1 += 1}`)
     player1Num3.setAttribute('value', `${valueChoiced3Player1 += 1}`)
+    
     player2Num1.setAttribute('value', `${valueChoiced1Player2 += 1}`)
     player2Num2.setAttribute('value', `${valueChoiced2Player2 += 1}`)
     player2Num3.setAttribute('value', `${valueChoiced3Player2 += 1}`)
-    
-
-})
-
-numChoicedPlay2.addEventListener('click', () => {
-
-    player1Num1.disabled = true
-    player1Num2.disabled = true
-    player1Num3.disabled = true
-
-    player2Num1.disabled = false
-    player2Num2.disabled = false
-    player2Num3.disabled = false
-    
+    numeroPrincipal += 1
+    verificarIs21(numeroPrincipal)
+}
+function somarButtons2(){
     player1Num1.setAttribute('value', `${valueChoiced1Player1 += 2}`)
     player1Num2.setAttribute('value', `${valueChoiced2Player1 += 2}`)
     player1Num3.setAttribute('value', `${valueChoiced3Player1 += 2}`)
+    
     player2Num1.setAttribute('value', `${valueChoiced1Player2 += 2}`)
     player2Num2.setAttribute('value', `${valueChoiced2Player2 += 2}`)
     player2Num3.setAttribute('value', `${valueChoiced3Player2 += 2}`)
+    numeroPrincipal += 2
+    verificarIs21(numeroPrincipal)
+}
+
+function somarButtons3() {
+    player1Num1.setAttribute('value', `${valueChoiced1Player1 += 3}`)
+    player1Num2.setAttribute('value', `${valueChoiced2Player1 += 3}`)
+    player1Num3.setAttribute('value', `${valueChoiced3Player1 += 3}`)
     
-})
+    player2Num1.setAttribute('value', `${valueChoiced1Player2 += 3}`)
+    player2Num2.setAttribute('value', `${valueChoiced2Player2 += 3}`)
+    player2Num3.setAttribute('value', `${valueChoiced3Player2 += 3}`)
+    numeroPrincipal += 3
+    verificarIs21(numeroPrincipal)
+}
 
-numChoicedPlay3.addEventListener('click', () => {
 
-    player1Num1.disabled = false
-    player1Num2.disabled = false
-    player1Num3.disabled = false
 
-    player2Num1.disabled = true
-    player2Num2.disabled = true
-    player2Num3.disabled = true
-   
-    player1Num1.setAttribute('value', `${valueChoiced1Player1 += 2}`)
-    player1Num2.setAttribute('value', `${valueChoiced2Player1 += 2}`)
-    player1Num3.setAttribute('value', `${valueChoiced3Player1 += 2}`)
-    player2Num1.setAttribute('value', `${valueChoiced1Player2 += 2}`)
-    player2Num2.setAttribute('value', `${valueChoiced2Player2 += 2}`)
-    player2Num3.setAttribute('value', `${valueChoiced3Player2 += 2}`)
+function verificarIs21() {
+  
+    console.log(numeroPrincipal)
+    if(numeroPrincipal === 21) {
+        sectionWinner.style.display = "flex"
+        numeroPrincipal = 0
+        
+        player1Num1.setAttribute('value', `${valueChoiced1Player1 = 1}`)
+        player1Num2.setAttribute('value', `${valueChoiced2Player1 = 2}`)
+        player1Num3.setAttribute('value', `${valueChoiced3Player1 = 3}`)
+        
+        player2Num1.setAttribute('value', `${valueChoiced1Player2 = 1}`)
+        player2Num2.setAttribute('value', `${valueChoiced2Player2 = 2}`)
+        player2Num3.setAttribute('value', `${valueChoiced3Player2 = 3}`)
+    }
+    else {
+
+    }
+}
+
+
+player1Num1.onclick = function(){
     
-})
-*/
+    somarButtons()
+    desligarBut1()
+    ativarBut2()
+    
+}
+
+player1Num2.onclick = function(){
+    
+    somarButtons2()
+    desligarBut1()
+    ativarBut2()
+
+}
+
+player1Num3.onclick = function(){
+    
+    somarButtons3()
+    desligarBut1()
+    ativarBut2()
+
+}
+
+player2Num1.onclick = function(){
+    
+    somarButtons()
+    desligarBut2()
+    ativarBut1()
+
+}
+
+player2Num2.onclick = function(){
+    
+    somarButtons2()
+    desligarBut2()
+    ativarBut1()
+
+}
+
+player2Num3.onclick = function(){
+    
+    somarButtons3()
+    desligarBut2()
+    ativarBut1()
+
+}
+
+
+
+
+
+
+
+
 
 
 
