21190c56b5805d9e4db3a3b8c24e29da91ad9058
FelipeHardmann <fashardmann@gmail.com>
2022-06-10 18:08:34 -0300

    Mudança axios

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -5,38 +5,37 @@ var buttonPlay = document.querySelector('.btnAvanced')
 
 //LocalStorage
 localStorage.setItem('inputPlayer', inputPlayer.value)
-let namePlayer1 = localStorage.getItem('inputPlayer')
+var namePlayer1 = localStorage.getItem('inputPlayer')
 
 localStorage.setItem('inputPlayer2', inputPlayer2.value)
-let namePlayer2 = localStorage.getItem('inputPlayer2')
-
+var namePlayer2 = localStorage.getItem('inputPlayer2')
+var url = "/html/Multiplayer.html"
 //Fuctions
+
+function responseNamePlayer2() {
+    axios.get(`https://api.github.com/users/${inputPlayer2.value}`)
+    .then((res) => {
+        console.log(res);
+        window.open(url)
+        window.focus
+    })
+    .catch(() => {
+        alert(`O Github ${inputPlayer2.value} não existe!`)
+    })
+}
 
 
     buttonPlay.onclick = function functionAxios() {
-        var url = "/html/Multiplayer.html"
+        
             axios.get(`https://api.github.com/users/${inputPlayer.value}`)
         .then(res =>{
-            window.open(url)
-            window.focus
+            console.log(res);
+            responseNamePlayer2() 
         })
         .catch(() => {
             alert(`O GitHub ${inputPlayer.value} não existe!`);
-        })
-
-        
-            axios.get(`https://api.github.com/users/${inputPlayer2.value}`)
-        .then(res =>{
-            window.open(url)
-            window.focus
-        })
-        .catch(() => {
-            alert(`O GitHub ${inputPlayer2.value} não existe!`);
             
         })
+
+          
     }
-
-     
-
-
-
